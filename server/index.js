24861bbd3e705f01b2226d124ec59f3c215ddca6
Julio Soto <juliosotodev@gmail.com>
2022-10-08 23:53:19 -0600

    added read endpoint

--- server/index.js
+++ server/index.js
@@ -8,10 +8,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/MERN-starter',
 )
 
 app.get('/insert', async (req,res) => {
-    const friend = new FriendModel({name: 'Pedro',age: 27})
+    const friend = new FriendModel({name: 'John',age: 21})
     await friend.save()
     res.send('Inserted friend data')
 })
 
-
+app.get('/read', (req,res) => {
+    FriendModel.find({}, (error,result) => {
+        if(error) res.send(error)
+            else res.send(result)
+    })
+})
 app.listen(3001, _=> console.log('Server Listening at PORT:3001'))
