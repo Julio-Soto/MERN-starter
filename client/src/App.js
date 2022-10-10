df0ea5b1eae0b4229e605cdd0e3312d6028d29a4
Julio Soto <juliosotodev@gmail.com>
2022-10-09 23:06:23 -0600

    finished input boxes and added submit button

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name,setName] = useState('')
+  const [age,setAge] = useState(0)
+
+  const handleSubmit = _=> alert(name + " " + age)
+
   return (
     <div className="App">
-      <input type='text'></input>
-      <input type='number'/>
+      <input 
+        type='text'
+        placeholder='First Name'
+        onChange={ (event) => setName(event.target.value)}
+       />
+      <input 
+        type='number'
+        placeholder='Age'
+        onChange={(event) => setAge(event.target.value)}
+      />
+      <button onClick={handleSubmit}>SUBMIT</button>
     </div>
   );
 }
